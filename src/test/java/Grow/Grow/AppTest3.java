6960487ce41f32e272d04a78a2c8ed1c2c527c87
amar mali <amardmali11@gmail.com>
2024-08-01 19:41:33 +0530

    new

---
+++ src/test/java/Grow/Grow/AppTest3.java
@@ -0,0 +1,15 @@
+package Grow.Grow;
+
+import org.testng.annotations.Test;
+
+public class AppTest3 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.out.println("App Test");
+    }
+}
