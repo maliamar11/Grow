1b12a9a38e56c8ec6dcf7b9470086b3162d208b7
amar mali <amardmali11@gmail.com>
2024-08-01 19:09:22 +0530

    First commit

---
+++ src/test/java/Grow/Grow/AppTest.java
@@ -0,0 +1,15 @@
+package Grow.Grow;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.out.println("App Test");
+    }
+}
